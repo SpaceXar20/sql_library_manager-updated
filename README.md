d58cfa886c32e156847b504d03a2acdd863f0b57
Erk L <ericklo1094011@gmail.com>
2019-02-10 01:28:31 -0500

    I initialized sequelize and created a model called book.js

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # sql_library_manager-updated
 This project is made with JavaScript, Node.js, Express, Pug, and the SQL ORM Sequelize.
 
-Test see if the repo is on the desktop app
+To run this app, type "npm install" in the terminal to install dependencies,
+then type npm start to run the website in the browser on localhost:3000
---
+++ models/book.js
@@ -0,0 +1,30 @@
+/*this is the Book model, the attributes here have the same name
+as the columns from the Books table from "library.db"*/
+
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const Book = sequelize.define('Book', {
+    title: {
+      type:  DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "Must have a title!" //validate title and author so that they can't be submitted blank
+        }
+      }
+    }, 
+    author:{
+      type:  DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: "The author's name is needed!"
+        }
+      }
+    },
+    genre: DataTypes.STRING,
+    year: DataTypes.INTEGER
+  }, {});
+  Book.associate = function(models) {
+    // associations can be defined here
+  };
+  return Book;
+};
